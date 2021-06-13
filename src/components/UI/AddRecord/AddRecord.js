3363ec7e3936c0eac74e268ccf777c640297cf79
Batshevag <79784318+Batshevag@users.noreply.github.com>
2021-06-13 12:09:31 +0300

    sound-box

---
+++ src/components/UI/AddRecord/AddRecord.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import './AddRecord.css'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+
+export default function AddRecord(props) {
+    const history = useHistory();
+    const [currentComponent, setCurrentComponent] = useState(0);
+    const userName = useSelector((state) => state.user.userName);
+    const currentChannel = useSelector(state => state.channel.currentChannel.name);
+    const channelName = useParams().ChannelName;
+
+    const nextCurrentComponent = () => {
+        setCurrentComponent(currentComponent + 1);
+    }
+
+    const currentPage = () => {
+        const allChildProps = { nextCurrentComponent, routingChange };
+        const child = React.Children.toArray(props.children)[currentComponent];
+        return React.cloneElement(child, allChildProps);
+    }
+    const routingChange = (page) => {
+        setCurrentComponent(page)
+    }
+    // const goMyChannel = (page) => {
+    //     history.push(`/admin/${userName}/${currentChannel}`)
+    // }
+    const goMyChannel = () => {
+        history.push(`/admin/${userName}/${channelName}`)
+    }
+     
+    return (
+        <div className="backgroundImage" >
+
+            <div className="d-flex align-items-start MyChannel"
+                onClick={goMyChannel}
+            >
+                {currentComponent === 0 && <ArrowBackIosIcon className="MyChannelIcn" />}
+                {currentComponent === 0 ? "My channel" : ""}
+            </div>
+            <div className="d-flex align-items-start flex-column">
+                <div className="wrap_body">
+                    {currentPage(currentComponent)}
+                </div>
+            </div>
+        </div>
+    );
+}
+
---
+++ src/components/UI/SingleAudio/Option/podcastSharingOption/podcastSharingOption.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
+import Sharing from '../Sharing';
+import Download from '../Download';
+import Favorite from '../Favorite';
+ import './podcastSharingOption.css'
+
+export default function podcastSharingOption(props) {
+  return (
+    <>
+      <div className="d-flex justify-content-center align-item-center">
+        <Sharing
+          className="IconSharing"
+          url={props.url} />
+
+        <Favorite
+          className="IconSharing Favorite"
+          id={props.id}
+        />
+
+  <Download
+           urlAudio={props.urlAudio}
+           titleAudio={props.titleAudio}
+          className="IconSharing" 
+   />
+      </div>
+    </>
+
+  );
+
+}
